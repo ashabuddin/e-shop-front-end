70ad8ab07b574af4aed352c77a40855ff4bf28de
Ashab <ashabuddin54321@gmail.com>
2021-11-02 12:35:09 +0600

    prod

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,9 +1,24 @@
-import axios from "axios";
+// import axios from "axios";
+
+// const BASE_URL = "http://localhost:5000/api/";
+// const TOKEN =
+//   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
+//     .accessToken || "";
+
+// export const publicRequest = axios.create({
+//   baseURL: BASE_URL,
+// });
+
+// export const userRequest = axios.create({
+//   baseURL: BASE_URL,
+//   header: { token: `Bearer ${TOKEN}` },
+// });
+
+ import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
 const TOKEN =
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNWU5MjIxOThlNTNiM2U3YjMzNTU5YiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzNDY0OTc3NiwiZXhwIjoxNjM0OTA4OTc2fQ.Ryc_l2Ibnydo5Cn-UiC-ys5-F5CJQcz9X2htQMhoJIQ"
-
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNWU5MjIxOThlNTNiM2U3YjMzNTU5YiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzNTU3MjE5MSwiZXhwIjoxNjM1ODMxMzkxfQ.kS4POLj0Lf2XFvGSE-kMFpGbRe3V6ovwuigXaeRLFb4"
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
